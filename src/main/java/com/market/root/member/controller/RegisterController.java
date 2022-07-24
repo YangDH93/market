75ecf5cba21d8b58b6fe0b5fcfbf63e824d986ea
Punghwi <0005cat@gmail.com>
2022-07-24 18:10:31 +0900

    회원가입에러수정

--- src/main/java/com/market/root/member/controller/RegisterController.java
+++ src/main/java/com/market/root/member/controller/RegisterController.java
@@ -41,9 +41,7 @@ public class RegisterController {
 		map.get("nickname");
 		map.get("email");
 		
-		/*
 		int result = rs.register(dto);
-		
 		
 		//회원가입 성공시 로그인 화면으로 이동
 		if(result == 1) {
@@ -51,9 +49,6 @@ public class RegisterController {
 		}
 		//회원가입 실패시 회원가입 창으로 다시 이동
 		return "redirect:register";
-		*/
-		
-		return "redirect:/";
 	}
 	
 	// 중복체크
